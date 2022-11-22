8756cf6563ae7949ee48dafa9a0714f8d0f24d58
Joseph Maramba <josephmaramba50@gmail.com>
2022-11-22 15:49:32 +0300

    calculator functionality

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,47 @@
 import '@fontsource/space-mono/700.css';
 import './styles/style.scss';
 
-const calculatorTipBtns = document.querySelectorAll<HTMLButtonElement>(
+const tipBtns = document.querySelectorAll<HTMLButtonElement>(
   '.calculator-tip-btns button'
 )!;
+const noOfPeopleInput = document.querySelector('.calculator-people input');
+const tipAmountOutput = document.querySelectorAll<HTMLParagraphElement>(
+  '.calculator-output p'
+)[0]!;
+const amountPerPersonOutput = document.querySelectorAll<HTMLParagraphElement>(
+  '.calculator-output p'
+)[1]!;
 
-calculatorTipBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    console.log('click');
+function calculateBill(value: number, tipPercent: number, noOfPeople: number) {
+  const tipAmount = ((value / noOfPeople) * tipPercent) / 100;
+  const amoutPerPerson = value / noOfPeople + tipAmount;
+  return [tipAmount.toFixed(2), amoutPerPerson.toFixed(2)];
+}
+
+let tipPercent: number;
+
+tipBtns.forEach((btn) => {
+  btn?.addEventListener('click', () => {
+    tipPercent = Number(btn.value);
   });
+});
+
+noOfPeopleInput?.addEventListener('keyup', () => {
+  const billValue = Number(
+    document.querySelector<HTMLInputElement>('.calculator-bill input')!.value
+  );
+  const noOfPeople = Number(
+    document.querySelector<HTMLInputElement>('.calculator-people input')!.value
+  );
+
+  const [tipAmount, amoutPerPerson] = calculateBill(
+    billValue,
+    tipPercent,
+    noOfPeople
+  );
+
+  if (tipAmount && amoutPerPerson) {
+    tipAmountOutput.textContent = `$${tipAmount}`;
+    amountPerPersonOutput.textContent = `$${amoutPerPerson}`;
+  }
 });
